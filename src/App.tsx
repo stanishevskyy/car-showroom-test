ac0a92409465c04d7701e977ec085d74d4755999
Taras Stanishevskyy <stanishevskyy98@gmail.com>
2026-07-22 19:53:57 +0300

    feat: implement footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
+
 import { Header } from './shared/components/Header';
+import { Footer } from './shared/components/Footer';
 
 export const App = () => {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
